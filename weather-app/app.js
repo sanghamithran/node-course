b6d159251647e27becccd84f3877db99e7c28121
Sanghamithran Sahadevan <Sanghamithran.Sahadevan@experian.com>
2019-08-06 17:44:17 +0530

    4. Making HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,10 @@
-console.log("Starting");
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+const request = require('request');
 
-setTimeout(() => {
-    console.log("2 Second timer");
-}, 2000);
+const url = 'https://api.darksky.net/forecast/0ee08d8f805d9445fbcd330617b763c5/37.8267,-122.4233';
 
-setTimeout(() => {
-    console.log("0 Second timer");
-}, 0)
 
-console.log("Stopping");
+request({url: url}, (error, response)=>{
+    const data = JSON.parse(response.body)
+    console.log(data.currently.time);
+});
