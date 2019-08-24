ed3dd0627b275afff739170da1e1da3c89c29bb2
Sanghamithran S <sanghamithran@gmail.com>
2019-08-25 04:29:29 +0530

    4. ES6 Aside Default Function Parameters

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -49,7 +49,7 @@ app.get('/weather', (req, res)=>{
             error: 'You must provide an address!!'
         })
     }
-    geocode(req.query.address, (error, { latitude , longitude, location } ) => {
+    geocode(req.query.address, (error, { latitude , longitude, location }= {} ) => {
         if(error){
             return res.send({ error })
         } 
