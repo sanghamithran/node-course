6249bf342020ffbe2da1d303ca78d224c7a95321
Sanghamithran S <sanghamithran@gmail.com>
2019-08-26 01:40:23 +0530

    6. Creating a Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -5,13 +5,25 @@ fetch('http://puzzle.mead.io/puzzle').then((response)=>{
     })
 })
 
-fetch('http://localhost:3000/weather?address=boston').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error);
-        }else{
-            console.log(data.location);
-            console.log(data.forecast);
-        }
+
+
+const weatherForm = document.querySelector('form');
+const search =  document.querySelector('input');
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const location = search.value;
+
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error){
+                console.log(data.error);
+            }else{
+                console.log(data.location);
+                console.log(data.forecast);
+            }
+        })
     })
+
 })
