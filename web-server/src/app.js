ba477934d069737f0d14b599fefc55dc01a2e039
Sanghamithran S <sanghamithran@gmail.com>
2019-09-03 02:42:39 +0530

    Path change

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,8 +3,8 @@ const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
 
-const geocode = require('../utils/geocode');
-const forecast = require('../utils/forecast');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 
