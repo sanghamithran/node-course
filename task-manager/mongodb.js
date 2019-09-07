37e6325f4b78fff48e6184a026ffaa8eae2eea4f
Sanghamithran S <sanghamithran@gmail.com>
2019-09-08 02:47:29 +0530

    9. Querying Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,64 +9,41 @@ const { MongoClient, ObjectID } = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const database = 'task-manager';
 
-const id = new ObjectID();
-console.log(id.id);
-console.log(id.toHexString().length);
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client)=>{
     if(error){
         return console.log('Unable to connect to the DB');
     }
 
-    // const db = client.db(database);
+    const db = client.db(database);
 
-    // db.collection('users').insertOne({
-    //     _id: id,
-    //     name: 'Vikram',
-    //     age: 26
-    // },(error, result)=>{
+    // db.collection('users').findOne({ _id: new ObjectID("5d72dddf99f1104ea4708a28") },(error, user)=>{
     //     if(error){
-    //         return console.log('Unable to insert user');
+    //         return console.log('Unable to fetch');
     //     }
 
-    //     console.log(result.ops);
+    //     console.log(user);
+    // })
+
+    // db.collection('users').find({ age: 27}).toArray((error, users)=>{
+    //     console.log(users);
+    // });
+     
+    // db.collection('users').find({ age: 27}).count((error, count)=>{
+    //     console.log(count);
     // });
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Jen',
-    //         age: 28
-    //     }, {
-    //         name: 'Gunther',
-    //         age: 27
-    //     }
-    // ],(error, result)=>{
-    //     if(error){
-    //         return console.log('Unable to insert users');
-    //     }
+    db.collection('tasks').findOne({_id: ObjectID("5d72d9cf782549ac345f4812")},(error, task)=>{
+        if(error){
+            return console.log(error);
+        }
 
-    //     console.log(result.ops);
-    // })
+        console.log(task);
+    });
 
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description:'Clean the house',
-    //         completed: true
-    //     },
-    //     {
-    //         description: 'Renew inspection',
-    //         completed: false
-    //     },
-    //     {
-    //         description: 'Pot plants',
-    //         completed: false
-    //     }
-    // ], (error, result)=>{
-    //     if(error){
-    //         return console.log('Unable to insert tasks');
-    //     }
+    db.collection('tasks').find({completed: false}).toArray((error, tasks)=>{
+        console.log(tasks);
+    })
 
-    //     console.log(result.ops);
-    // })
 
 });
