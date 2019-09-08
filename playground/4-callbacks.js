96d56ed1034dbd83a90d714fc8007d70ff53eb56
Sanghamithran S <sanghamithran@gmail.com>
2019-09-09 02:37:15 +0530

    10. Promises

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -30,12 +30,29 @@
 // 3. After 2 seconds are up, call the callback function with the sum
 // 4. Test your work!
 
-const add = (a, b, callback) =>{
-    setTimeout(() =>{
-        callback(a + b);
-    }, 2000)
+// const add = (a, b, callback) =>{
+//     setTimeout(() =>{
+//         callback(a + b);
+//     }, 2000)
+// }
+
+// add(1, 4, (sum) => {
+//     console.log(sum) // Should print: 5
+// })
+
+
+const doWorkCallback = (callback)=>{
+    setTimeout(()=>{
+        // callback("This is my error", undefined);
+        callback(undefined, [1,4,7]);
+    }, 2000);
 }
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
+
+doWorkCallback((error, result)=>{
+    if (error) {
+        return console.log(error);
+    }
+
+    console.log(result);
 })
