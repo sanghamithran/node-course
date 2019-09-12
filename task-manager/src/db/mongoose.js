92ef2fd6f237bfb496ea479a2c9974d6d91e91a1
Sanghamithran S <sanghamithran@gmail.com>
2019-09-13 03:42:38 +0530

    10. Resource Reading Endpoints Part I

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-
 mongoose.connect('mongodb+srv://san:san@cluster0-tiacs.mongodb.net/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
