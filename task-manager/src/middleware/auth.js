b7c349f1eb37dc89118cd4d45d272822dcd37e3c
Sanghamithran S <sanghamithran@gmail.com>
2019-11-24 04:29:58 +0530

    8. Accepting Authentication Tokens

---
+++ task-manager/src/middleware/auth.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-underscore-dangle */
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, 'thismynewcourse');
+    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).send('Please authenticate');
+  }
+};
+
+module.exports = auth;
