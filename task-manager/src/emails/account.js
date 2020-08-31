4fbe0ee971678fe33ef9628bdbd2edd5b7311739
Sanghamithran S <sanghamithran@gmail.com>
2020-09-01 04:09:56 +0530

    3. Sending Welcome and Cancelation Emails

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -5,9 +5,25 @@ const mailgunDomain = 'sandbox381521aaef9045bcae4be0332e3159a1.mailgun.org';
 
 const mg = mailgun({ apiKey: mailgunAPIKey, domain: mailgunDomain });
 
-mg.messages().send({
-  from: 'San <luttappino1@gmail.com>',
-  to: 'luttappino1@gmail.com',
-  subject: 'This is my first creation!',
-  text: 'I hope this one actually get to you',
+const sendWelcomeEmail = (email, name) => {
+  mg.messages().send({
+    from: 'San <luttappino1@gmail.com>',
+    to: email,
+    subject: 'Thanks for joining in!',
+    text: `Welcome to the app ${name}. Let me know how you get along with the app.`,
+  });
+};
+
+const sendCancellationEmail = (email, name) => {
+  mg.messages().send({
+    from: 'San <luttappino1@gmail.com>',
+    to: email,
+    subject: 'Sorry to see you go!',
+    text: `Goodbye, ${name}. I hope to see you soon.`,
+  });
+};
+
+module.exports = ({
+  sendWelcomeEmail,
+  sendCancellationEmail,
 });
