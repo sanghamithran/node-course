282d0ecc8c82af50edcb45cb66245190e78606f8
Sanghamithran S <sanghamithran@gmail.com>
2020-09-08 03:26:45 +0530

    4. Writing Your Own Tests

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -1,5 +1,18 @@
 const calculateTip = (total, tipPercent = 0.25) => total + (total * tipPercent);
+const fahrenheitToCelsius = (temp) => (temp - 32) / 1.8;
+
+const celsiusToFahrenheit = (temp) => (temp * 1.8) + 32;
+
+//
+// Goal: Test temperature conversion functions
+//
+// 1. Export both functions and load them into test suite
+// 2. Create "Should convert 32 F to 0 C"
+// 3. Create "Should convert 0 C to 32 F"
+// 4. Run the Jest to test your work!
 
 module.exports = {
   calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit,
 };
