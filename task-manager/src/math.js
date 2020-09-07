1c7643a885d41ae3e00019c0ba50099bb80f2b57
Sanghamithran S <sanghamithran@gmail.com>
2020-09-08 03:54:09 +0530

    5. Testing Asynchronous Code

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -3,16 +3,19 @@ const fahrenheitToCelsius = (temp) => (temp - 32) / 1.8;
 
 const celsiusToFahrenheit = (temp) => (temp * 1.8) + 32;
 
-//
-// Goal: Test temperature conversion functions
-//
-// 1. Export both functions and load them into test suite
-// 2. Create "Should convert 32 F to 0 C"
-// 3. Create "Should convert 0 C to 32 F"
-// 4. Run the Jest to test your work!
+const add = (a, b) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (a < 0 || b < 0) {
+      return reject('Numbers must be non-negative');
+    }
+
+    resolve(a + b);
+  }, 2000);
+});
 
 module.exports = {
   calculateTip,
   fahrenheitToCelsius,
   celsiusToFahrenheit,
+  add,
 };
