ca8aa61a121d7c0f79167a44fa8c8e5a426cfe13
Sanghamithran S <sanghamithran@gmail.com>
2020-12-14 02:26:14 +0530

    10. Advanced Assertions

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-undef */
 const request = require('supertest');
 const jwt = require('jsonwebtoken');
@@ -22,18 +23,34 @@ beforeEach(async () => {
 });
 
 test('Should sign up a new user', async () => {
-  await request(app).post('/users').send({
+  const response = await request(app).post('/users').send({
     name: 'Andrew',
     email: 'andrew@example.com',
     password: 'MyPass777!',
   }).expect(201);
+
+  // Assert that database is changed correctly
+  const user = await User.findById(response.body.user._id);
+  expect(user).not.toBeNull();
+
+  // Assertion about the response
+  expect(response.body).toMatchObject({
+    user: {
+      name: 'Andrew',
+      email: 'andrew@example.com',
+    },
+    token: user.tokens[0].token,
+  });
+  expect(user.password).not.toBe('MyPass777!');
 });
 
 test('Should login existing user', async () => {
-  await request(app).post('/users/login').send({
+  const response = await request(app).post('/users/login').send({
     email: userOne.email,
     password: userOne.password,
   }).expect(200);
+  const user = await User.findById(userOneId);
+  expect(response.body.token).toBe(user.tokens[1].token);
 });
 
 test('Should not login nonexistent user', async () => {
@@ -64,6 +81,8 @@ test('Should delete account for user', async () => {
     .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
     .send()
     .expect(200);
+  const user = await User.findById(userOneId);
+  expect(user).toBeNull();
 });
 
 test('Should not delete account for unauthenticated user', async () => {
