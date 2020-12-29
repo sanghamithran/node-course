76b50fb1d03a07d6ff02b3a4770536cf0dc29fbc
Sanghamithran S <sanghamithran@gmail.com>
2020-12-30 03:50:03 +0530

    5. Socket.io Events.mp4

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,8 +12,17 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+var count = 0;
+
+io.on('connection', (socket) => {
   console.log('New WebSocket connection');
+
+  socket.emit('countUpdated', count);
+  socket.on('increment', () => {
+    count++;
+    // socket.emit('countUpdated', count);
+    io.emit('countUpdated', count);
+  });
 });
 
 server.listen(port, () => {
