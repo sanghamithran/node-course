d310cf04fd7b47bf376b7fdfeb997fb36c0e20e4
Sanghamithran S <sanghamithran@gmail.com>
2021-04-21 03:45:16 +0530

    8. Sharing Your Location

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -11,3 +11,15 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 
   socket.emit('sendMessage', message);
 });
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  });
+});
